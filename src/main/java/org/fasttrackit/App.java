e598c2f05f0a6499597980d9df1ebad6f186042f
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-11-15 13:06:19 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.name = "Golf";
+        car.doorCount = 2;
+
+        System.out.println(car.name);
+    }
+}
---
+++ src/main/java/org/fasttrackit/Driver.java
@@ -0,0 +1,9 @@
+package org.fasttrackit;
+
+public class Driver {
+
+    String name;
+    double age;
+    double height;
+
+}
